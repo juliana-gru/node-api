f0cf87a5a351f9da20d35acf86c1e081279b4533
Juliana Grutzmann <juliana_grutzmann@hotmail.com>
2020-08-23 10:25:38 +1000

    new ProductController

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 //Iniciando o app
 const app = express();
 
 //Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true, useNewUrlParser:  true });
+require('./src/models/Products');
 
-//Primeira rota
-app.get('/', (request, response) => {
-  response.send('Hello Rocketseat');
-});
+//Rotas
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
