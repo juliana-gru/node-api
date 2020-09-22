1f6c1f0bbffe6d85f5b908b0ddb2bdf7e389fbc0
Juliana Grutzmann <juliana_grutzmann@hotmail.com>
2020-09-22 16:50:22 +1000

    add cors and pagination

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 //Iniciando o app
 const app = express();
 
-//Permite que eu envie dados da app no formato de json
-app.use(express.json());
+//Middlewares
+app.use(cors()); //allow cross origen access
+app.use(express.json());//Permite que eu envie dados da app no formato de json
 
 //Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true, useNewUrlParser:  true });
